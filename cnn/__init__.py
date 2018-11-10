198c35ee5d060c64e4ac0e16ab7da97f2e038bb7
StevenXue <15994039246@163.com>
2018-11-10 09:53:38 +0800

    卷积神经网络&朴素贝叶斯

---
+++ cnn/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-  
+"""  
+  @desc:  
+  @author: StevenXue 
+  @date: 2018/11/5
+"""
+
+
+class MyClass(Exception):
+    """
+    Demo for template
+    """
+    pass
+
+
+def test():
+    print('hello')
+    pass
+
+
+if __name__ == "__main__":
+    test()
